2e3a63e0d28aaf858f6ce0b07b28f3dc03d5ed53
tomoki matsuoka <tomoki.matsuoka@gmail.com>
2020-06-27 00:59:40 +0900

    Create LCD_TEST.ino
    LCDのテストプログラムです。Ascii文字を順番に表示します。

---
+++ LCD_TEST.ino
@@ -0,0 +1,46 @@
+// LCD TEST PROGRAM 
+// https://github.com/matsuoki/cwdecoder_ja.ino/
+
+#include <LiquidCrystal_I2C.h>
+const int LCD_COLUMNS = 20;
+const int LCD_ROWS = 4;
+int c = 0;
+int r = 0;
+int cnt = 0;
+
+
+LiquidCrystal_I2C lcd(0x27, LCD_COLUMNS, LCD_ROWS);
+
+void setup() {
+  Serial.begin(115200);
+
+  lcd.init();
+  lcd.backlight();
+  lcd.clear();
+  lcd.begin(LCD_COLUMNS, LCD_ROWS);
+
+  Serial.println("Start");
+  lcd.setCursor(1, 1);
+  lcd.print("hello.");
+}
+
+void loop() {
+  Serial.print(c);
+  Serial.print(",");
+  Serial.print(r);
+  Serial.print(" --- ");
+  Serial.println( cnt ) ;
+
+  lcd.setCursor(c, r);
+  lcd.print(char(cnt));
+  cnt++;
+  c++;
+  if ( c >= LCD_COLUMNS ) {
+    c = 0;
+    r++;
+    if ( r >= LCD_ROWS) {
+      r = 0;
+    }
+  }
+  delay(100);
+}
